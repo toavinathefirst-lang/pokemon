845366bc27ba889180c05659fad161e0b73c7ac9
toavinathedev-luffy <toavinathefirst@gmail.com>
2026-09-18 13:19:57 +0300

    ajout de map et de tileset

---
+++ assets/pokemon style game.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="pokemon style game" tilewidth="12" tileheight="12" tilecount="1024" columns="32">
+ <image source="Chris Courses - Pokemon-20260818T134009Z-1-001/Chris Courses - Pokemon/Images/sceneSpriteSheet.png" width="384" height="384"/>
+</tileset>

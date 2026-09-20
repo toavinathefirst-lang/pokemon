fc1b1fe62c3e1aaf9023e1a84467ab339d969ab6
toavinathedev-luffy <toavinathefirst@gmail.com>
2026-09-20 20:11:01 +0300

    ajout de collisionner et la map est pellet town

---
+++ assets/CollisionTileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="Collision" tilewidth="12" tileheight="12" tilecount="1" columns="1">
+ <image source="Chris Courses - Pokemon-20260818T134009Z-1-001/Chris Courses - Pokemon/Tiled/collision.png" width="12" height="12"/>
+</tileset>

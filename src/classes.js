706a92a7fdd3ac68ed47efe3eb262a7def626d54
toavinathedev-luffy <toavinathefirst@gmail.com>
2026-09-21 01:41:07 +0300

    constructeur du sprite

---
+++ src/classes.js
@@ -0,0 +1,40 @@
+export class Sprite{
+    /**
+     * 
+     * @param {Object} options
+     * @param {{x:number,y:number}} options.position
+     * @param {{x:number,y:number}} options.velocity
+     * @param {HTMLImageElement} options.image
+     * @param {{max:number,hold:number}} [options.frames={max:1,hold:10}]
+     * @param {Object.<string, HTMLImageElement>} [options.sprites]
+     * @param {boolean} [options.animate=false]
+     * @param {number} [options.rotation=0]
+     * @param {number} [options.scale=1]
+     */
+    constructor({
+        position,
+        velocity,
+        image,
+        frames = { max: 1, hold: 10 },
+        sprites,
+        animate = false,
+        rotation = 0,
+        scale = 1
+    }){
+        this.position=position;
+        this.image=new Image()
+        this.frames = { ...frames, val: 0, elapsed: 0 }
+        this.image.onload =()=>{
+            this.width=(this.image.width / this.frames.max)*scale
+            this.height=this.image.height * scale
+        }
+          this.image.src = image.src
+
+        this.animate = animate
+        this.sprites = sprites
+        this.opacity = 1
+
+        this.rotation = rotation
+        this.scale = scale
+    }
+}

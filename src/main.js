d714ef6087505bfcd58fd43e46e2131841519ab8
toavinathedev-luffy <toavinathefirst@gmail.com>
2026-09-22 02:25:07 +0300

    ajout du personnage

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import './style.css'
 import petTownUrl from './assets/chrisCourseAssets/ChrisCoursesPokemon/Tiled/Pellet TownZoom.png'
-
+import playerDown from './assets/chrisCourseAssets/ChrisCoursesPokemon/Images/playerDown.png'
 const canvas = document.querySelector("canvas");
 const c = canvas.getContext('2d')
 
@@ -11,9 +11,17 @@ c.fillStyle="white"
 c.fillRect(0,0,canvas.width,canvas.height)
 const image = new Image();
 image.src = petTownUrl;
-
+const playerImage = new Image()
+playerImage.src =playerDown
 image.onload = () => {
     c.drawImage(image, -750, -550)
+    c.drawImage(playerImage,0,0,playerImage.width/4,
+        playerImage.height,canvas.width/2 -playerImage.width/4  /2 ,
+        canvas.height /2 - playerImage.height/2,
+        playerImage.width/4,
+        playerImage.height
+    )   
+
 }
 
 image.onerror = (e) => {
